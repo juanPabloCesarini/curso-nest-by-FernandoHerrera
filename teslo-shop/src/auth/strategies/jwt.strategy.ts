690361a0736035e9b3f05cf96e01fdd7b43cf518
JuanPabloCesarini <juanpcesarini@hotmail.com>
2025-12-29 17:33:49 -0300

    Continuando la seccion de autentication y autorizacion - JwtStrategy

---
+++ teslo-shop/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "../entities/auth.entity";
+import { JwtPayload } from "../interfaces/jwt-payload.interface";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository:Repository<User>,
+        private readonly configService:ConfigService
+        
+    ){
+        super({
+            secretOrKey:configService.get('JWT_SECRET') as string,
+            jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
+        });
+    }
+    async validate(payload: JwtPayload): Promise<User> {
+        const {email}=payload;
+
+        const user = await this. userRepository.findOneBy({email});
+
+        if (!user)
+            throw new UnauthorizedException('token not valid')
+
+        if (!user.isActive)
+            throw new UnauthorizedException('User is inactive, talk with admin')
+        return user;
+    }
+}
